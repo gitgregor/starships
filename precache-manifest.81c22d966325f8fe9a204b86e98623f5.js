4748581b80de7b087b6b5cf42459a1212e22db9e
Gregory <gerda.trust@gmail.com>
2020-01-25 18:34:15 +0100

    Updates

---
+++ precache-manifest.81c22d966325f8fe9a204b86e98623f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0779f6245e7a8bb00e9ac326715a1a0f",
+    "url": "/starships/index.html"
+  },
+  {
+    "revision": "ebae688491d28dc03924",
+    "url": "/starships/static/css/main.6ccf6dc7.chunk.css"
+  },
+  {
+    "revision": "8ed91cd23a894a44604e",
+    "url": "/starships/static/js/2.7be41522.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/starships/static/js/2.7be41522.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebae688491d28dc03924",
+    "url": "/starships/static/js/main.cf128d85.chunk.js"
+  },
+  {
+    "revision": "e20c63308d8a20908951",
+    "url": "/starships/static/js/runtime-main.909ccce6.js"
+  },
+  {
+    "revision": "ea69c3d7e2a8b2fe9ec1a79f54c658b4",
+    "url": "/starships/static/media/spaceShip1_1920.ea69c3d7.jpg"
+  }
+]);
